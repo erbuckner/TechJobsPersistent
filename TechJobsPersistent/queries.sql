2e9bd4a3f958a49ca6d651e921fd8b4e17a9dc2c
erbuckner <erbuckner@gmail.com>
2021-12-10 12:07:58 -0500

    * queries.sql: Complete SQL Task 3 and corrections
    * Index.cshtml: Change "Location:" label to "Skill:"

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,17 @@
 --Part 1
+SELECT column_name,data_type
+FROM information_schema.columns
+WHERE table_schema = 'techjobs' and table_name = 'Jobs';
 
 --Part 2
+SELECT Name
+FROM Employers
+WHERE (Location = 'St. Louis');
 
 --Part 3
-
+SELECT DISTINCT Skills.Name, Skills.Description
+FROM JobSkills
+RIGHT JOIN Skills
+ON JobSkills.SkillId = Skills.Id
+WHERE JobSkills.SkillId IS NOT NULL
+ORDER BY Skills.Name ASC
